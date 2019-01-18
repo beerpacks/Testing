aca15cd30a75d8e9f77fc9d7413594fe7428b0a6
jean-francois <jean-francois.pelletier@cafection.com>
2019-01-18 13:18:39 -0500

    commitReactNativeForms

--- webtutorial/coursera/Course3_ReactNative/confusion/components/MainComponent.js
+++ webtutorial/coursera/Course3_ReactNative/confusion/components/MainComponent.js
@@ -9,6 +9,7 @@ import {createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView} 
 import {Icon} from 'react-native-elements'
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
 
 const mapStateToProps = state => {
   return {
@@ -79,6 +80,22 @@ const ContactNavigator = createStackNavigator({
     })
 });
 
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }} 
+        onPress={ () => navigation.navigate('DrawerToggle') } />    
+    })
+  })
 const AboutNavigator = createStackNavigator({
     About: { screen: About }
 },
@@ -152,6 +169,21 @@ const MainNavigator = createDrawerNavigator({
                 <Icon name='address-card' type='font-awesome' size={22} color={tintColor}/>
             )
         }
+    } ,
+    Reservation:
+    { screen: ReservationNavigator,
+      navigationOptions: {
+        title: 'Reserve Table',
+        drawerLabel: 'Reserve Table',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='cutlery'
+            type='font-awesome'            
+            size={24}
+            iconStyle={{ color: tintColor }}
+          />
+        ),
+      }
     }
 },{
     drawerBackgroundColor:'#D1C4E9',
