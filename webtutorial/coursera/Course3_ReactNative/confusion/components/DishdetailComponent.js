8d223ca94f780970397fe736997c0225b55eeac6
jean-francois <jean-francois.pelletier@cafection.com>
2019-01-21 15:16:32 -0500

    commitCourse3_assignment2

--- webtutorial/coursera/Course3_ReactNative/confusion/components/DishdetailComponent.js
+++ webtutorial/coursera/Course3_ReactNative/confusion/components/DishdetailComponent.js
@@ -1,9 +1,9 @@
 import React,{Component} from 'react';
-import {View, Text, ScrollView, FlatList} from 'react-native';
-import {Card, Icon} from 'react-native-elements';
+import {View, Text, ScrollView, FlatList, Modal, Button,StyleSheet} from 'react-native';
+import {Card, Icon, Rating, FormLabel, FormInput} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -15,7 +15,8 @@ const mapStateToProps = state => {
   }
 
 const mapDispatchToProps = dispatch => ({
-    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+    postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+    postComment:(dishId, author, comment, rating)  => dispatch(postComment(dishId, author, comment, rating))
 })
 
 function RenderDish(props){
@@ -24,7 +25,10 @@ function RenderDish(props){
         return (
             <Card featuredTitle={dish.name} image={{uri: baseUrl + dish.image}}>
                 <Text style={{margin:10}}>{dish.description}</Text>
-                <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' onPress={()=>props.favorites ?console.log('already favorite'):props.onPress()}/>
+                <View style={{flexDirection:'row', flex:1, justifyContent:'center'}}>
+                    <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' onPress={()=>props.favorites ?console.log('already favorite'):props.onPress()}/>
+                    <Icon raised reverse name={'pencil'} type='font-awesome' color='#512DA8' onPress={()=>props.onNewComment()}/>
+                </View>
             </Card>
         );
     }else{
@@ -56,8 +60,34 @@ function RenderComments(props){
 
 class DishDetail extends Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            showModal:false,
+            rating:3,
+            comment:'',
+            author:''
+        }
+    }
+
     markFavorite(dishId) {
         this.props.postFavorite(dishId);
+    }
+
+    newComment(){
+        //alert(JSON.stringify(this.state));
+        this.setState({
+            showModal:!this.state.showModal
+        });
+    }
+
+    resetState(){
+        this.setState({
+            showModal:false,
+            rating:3,
+            comment:'',
+            author:''
+        });
     }
 
     static navigationOptions = {
@@ -70,12 +100,79 @@ class DishDetail extends Component {
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
                      favorite={this.props.favorites.some(el => el === dishId)}
-                    onPress={() => this.markFavorite(dishId)} 
+                     onPress={() => this.markFavorite(dishId)} 
+                     onNewComment={()=>this.newComment()}
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
+                <Modal 
+                    animationType = {"slide"} 
+                    transparent = {false}
+                    visible = {this.state.showModal}
+                    onDismiss = {() =>this.resetState() }
+                    onRequestClose = {() =>this.resetState() }>
+                    <View style = {styles.modal}>
+                        <Rating 
+                            style={{alignSelf:'center'}}
+                            showRating
+                            step
+                            onFinishRating={(rate)=>this.setState({rating:rate})}
+                        />
+                        <FormLabel>Author</FormLabel>
+                        <FormInput onChangeText={(text)=>this.setState({author:text})}/>
+                        <FormLabel>Comment</FormLabel>
+                        <FormInput onChangeText={(text)=>this.setState({comment:text})}/>
+                        <Button 
+                            onPress = {() =>
+                                {
+                                    this.props.postComment(dishId,this.state.author,this.state.comment,this.state.rating);
+                                    this.resetState();
+                                }}
+                            color="#512DA8"
+                            title="Submit" 
+                            />
+                        <Button
+                            onPress = {() =>this.resetState()}
+                            color="#512DA8"
+                            title="Cancel" 
+                            />
+                    </View>
+                </Modal>
             </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    formRow: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      flexDirection: 'row',
+      margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+     },
+     modalTitle: {
+         fontSize: 24,
+         fontWeight: 'bold',
+         backgroundColor: '#512DA8',
+         textAlign: 'center',
+         color: 'white',
+         marginBottom: 20
+     },
+     modalText: {
+         fontSize: 18,
+         margin: 10
+     }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(DishDetail);
--- webtutorial/coursera/Course3_ReactNative/confusion/redux/comments.js
+++ webtutorial/coursera/Course3_ReactNative/confusion/redux/comments.js
@@ -4,7 +4,17 @@ export const comments = (state = { errMess: null, comments:[]}, action) => {
   switch (action.type) {
     case ActionTypes.ADD_COMMENTS:
       return {...state, errMess: null, comments: action.payload};
-
+    case ActionTypes.ADD_COMMENT:
+      var tmpComment = {
+        author:action.payload.author,
+        comment:action.payload.comment,
+        date:action.payload.date,
+        dishId:action.payload.dishId,
+        id:state.comments.length,
+        rating:action.payload.rating              
+      };
+      return {...state,errMess:null,comments:state.comments.concat(tmpComment)}
+    
     case ActionTypes.COMMENTS_FAILED:
       return {...state, errMess: action.payload};
 
