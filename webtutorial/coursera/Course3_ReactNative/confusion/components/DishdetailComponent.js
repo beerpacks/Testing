13eed9072143774f6cac3b1f16804473fcdcc453
jean-francois <jean-francois.pelletier@cafection.com>
2019-01-24 14:18:07 -0500

    commitSocialSharing

--- webtutorial/coursera/Course3_ReactNative/confusion/components/DishdetailComponent.js
+++ webtutorial/coursera/Course3_ReactNative/confusion/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import {Card, Icon, Rating, FormLabel, FormInput} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -60,6 +60,16 @@ function RenderDish(props){
         }
     })
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -72,6 +82,14 @@ function RenderDish(props){
                     <View style={{flexDirection:'row', flex:1, justifyContent:'center'}}>
                         <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' onPress={()=>props.favorites ?console.log('already favorite'):props.onPress()}/>
                         <Icon raised reverse name={'pencil'} type='font-awesome' color='#512DA8' onPress={()=>props.onNewComment()}/>
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            //style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                     </View>
                 </Card>
             </Animatable.View>
