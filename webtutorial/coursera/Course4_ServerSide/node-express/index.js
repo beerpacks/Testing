6c45afa93786424679c1d64abf2955f3da826c1b
jean-francois <jean-francois.pelletier@cafection.com>
2019-01-28 15:00:04 -0500

    commitExpressRouterV2

--- webtutorial/coursera/Course4_ServerSide/node-express/index.js
+++ webtutorial/coursera/Course4_ServerSide/node-express/index.js
@@ -4,44 +4,14 @@ const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 const bodyParser = require('body-parser');
-
+const dishRouter = require('./routes/dishRouter');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use('/dishes',dishRouter);
 app.use(express.static(__dirname + '/public'));
-app.all('/dishes', (req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
-app.get('/dishes',(req,res,next)=>{
-    res.end('will send all the dishes to you !');
-});
-app.post('/dishes',(req,res,next)=>{
-    res.end('Will add the dish: ' + req.body.name + ' with details : ' +req.body.description );
-});
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-app.delete('/dishes',(req,res,next)=>{
-    res.end('deleting all the deshies !');
-});
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('will send '+ req.param.dishId+ ' the dishe to you !');
-});
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/:dishId('+req.param.dishId+')');
-});
-app.put('/dishes/:dishId',(req,res,next)=>{
-    res.write('Updating the dish ' + req.param.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details : '+ req.body.description);
-});
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end('Deleting dish:' + req.param.dishId);
-});
+
 
 app.use((req, res, next) => {
     console.log(req.headers);
