1b843d9f242d381f737725d289a1007f6e5a85ca
jean-francois <jean-francois.pelletier@cafection.com>
2019-01-31 11:57:45 -0500

    commitNodeAndMongoPart2.2

--- webtutorial/coursera/Course4_ServerSide/node-mongo/index.js
+++ webtutorial/coursera/Course4_ServerSide/node-mongo/index.js
@@ -4,36 +4,38 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err,client) =>{
+MongoClient.connect(url, (err, client) => {
 
-    assert.equal(err,null);
+    assert.equal(err, null);
 
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, {name:"Vadonut", description:"Test"}, 'dishes', (result) =>{
-        console.log('Insert Document:\n', result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        dboper.findDocuments(db, 'dishes',(docs)=>{
-            console.log('Found Documents:\n', docs);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-            dboper.updateDocument(db,{name:'Vadonut'},{description:'Updated Test'},'dishes',(result)=>{
-                console.log('Updated Document:\n', result.result);
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-                dboper.findDocuments(db,'dishes',(docs)=>{
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
 
-                    console.log('Find documents \n ', docs);
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
 
-                    db.dropCollection('dishes', (result)=>{
-                        console.log('Dropped collection: ', result);
-
-                        client.close();
+                                client.close();
+                            });
+                        });
                     });
-                });
             });
         });
-    });
 
 
 });
