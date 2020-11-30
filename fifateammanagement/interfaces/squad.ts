65e9b973aa29e1bc56d82e30a8cb93f82ab4ad24
Jean-Francois Pelletier <jeff.pelletier@cafection.com>
2020-11-30 08:07:11 -0500

    commitSquad2.0

--- fifateammanagement/interfaces/squad.ts
+++ fifateammanagement/interfaces/squad.ts
@@ -1,8 +1,16 @@
 import { BaseRequest, BaseResponse } from './base'
 
 export interface SquadPlayer {
+    uuid: string,
     name: string,
-    contractType: string
+    overall: number,
+    potentiel: number,
+    position: string,
+    country: string,
+    contractType: string,
+    age: number,
+    value: number,
+    wages: number
 }
 
 export interface GetSquadResponse extends BaseResponse {
